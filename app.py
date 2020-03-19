0a2ef74e6c788c423a6d62ed749b1455e3a7eaf3
brendanoshea84 <b_oshea_84@hotmail.com>
2020-03-19 11:42:22 +0100

    update login page, error passing from mongdb

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId 
+   
 
-app= Flask(__name__)
+app = Flask(__name__)
+app.config["MONGO_DBNAME"] = "milestone3"
+app.config["MONGO_URI"] = os.getenv('MONGO_URI')
+
+
+mongo = PyMongo(app)
 
 @app.route('/')
-def hello():
-    return 'Hello World'
+@app.route('/intro')
+def intro():
+    return render_template("intro.html", employees=mongo.db.employees.find_one())
 
 if __name__ == '__main__':
-    app.run(host=os.environ.get('IP'), 
-    port=os.environ.get('PORT'),
-    debug=True)
-   
+    app.run(host=os.environ.get('IP'),
+            port=os.environ.get('PORT'),
+            debug=True)  
