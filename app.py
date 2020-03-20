00b64ffd984b82dc162ccccb990abe449e8a7c56
brendanoshea84 <b_oshea_84@hotmail.com>
2020-03-20 12:43:53 +0100

    problems with POST

--- app.py
+++ app.py
@@ -1,36 +1,48 @@
 import os
-from flask import Flask, render_template, redirect, request, url_for, json
+from flask import Flask, render_template, redirect, request, url_for, session
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId 
-
-
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = "milestone3"
 app.config["MONGO_URI"] = os.getenv('MONGO_URI')
 
-
 mongo = PyMongo(app)
+
+
+
 
 @app.route('/')
 @app.route('/intro', methods=['POST', 'GET'])
-def intro():
+def new_member():
     if request.method == 'POST':
-        welcome_password = mongo.db.welcome_password
-        try_password = form.getvalue('new_password')
-
-        if welcome_password == try_password:
-            return render_template('new_member_info.html')
-
-
-    return render_template("intro.html", employees=mongo.db.employees.find())  
+        welcome_password = mongo.db.welcome_password.find()
+        
+        if welcome_password == request.form.get('new_password'): 
+            return redirect(url_for('base'))
+        
+    return render_template('intro.html', employees=mongo.db.employees.find(), welcomes=mongo.db.welcome_password.find())       
 
 
 
 
 
-@app.route('/new_member_info', methods=['POST', 'GET'])   
+
+
+
+@app.route('/base')
+def base():
+    print: "welcome"
+    return render_template("base.html")
+
+
+
+
+
+
+@app.route('/new_member_info')   
 def new_member_info():
+    print: "testing"
     return render_template("new_member_info.html")    
 
 if __name__ == '__main__':
