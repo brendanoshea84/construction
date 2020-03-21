2473de87c0eb9638bde89849a44ec083e2e30796
brendanoshea84 <b_oshea_84@hotmail.com>
2020-03-21 08:01:01 +0100

    login as new or exsiting member is working. Need to add error signs if username or password is wrong

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import os
+from tkinter import *
 from flask import Flask, render_template, redirect, request, url_for, session
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId 
+from tkinter import messagebox
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = "milestone3"
@@ -9,32 +11,47 @@ app.config["MONGO_URI"] = os.getenv('MONGO_URI')
 
 mongo = PyMongo(app)
 
+# Welcome Page
+# Sign in for new members with a pre password
+# Login using your own password
 @app.route('/', methods=['POST', 'GET'])
 @app.route('/intro', methods=['POST', 'GET'])
-def new_member():
-    
-
-    if request.method == 'GET':
-        print ("GET working")
-        
-        
+def new_member():   
+    # Sign in for new members with a pre password
     if request.method == 'POST':
         try_pass = request.form.get('new_password')
         w_password = mongo.db.welcome_password.find_one()
         for key, val in w_password.items():
             if 'welcome_password' in key:
                 wel_pass = val
-        print (wel_pass)
-        print ("POST working")
-        print ("test 2") 
-        print (try_pass)
-    
         if wel_pass == try_pass:
-            
-            print ("try and pass") 
-            print (w_password == try_pass)
+            # If sussecful- Page to input new member info
+            print("testing wel_pass == try_pass")
             return redirect(url_for('new_member_info'))
         
+
+   
+
+
+    # Login using your own password
+    if request.method == 'POST':
+        # User can use they name or employee number
+        employees = mongo.db.employees
+        login_user =  employees.find_one({'employee_username' : request.form.get('login')}) or employees.find_one({'employee_number' : request.form.get('login')})
+         
+        login_pass = request.form.get('password_user')
+
+        if login_user:
+            if login_pass == login_user['employ_password']:
+                print("testing login area")
+                session['login_user'] = request.form['login']
+                print(session)
+                print('session')
+                return redirect(url_for('base'))
+            return 'Error' 
+            print ("error stage 1") 
+              
+   
     return render_template('intro.html', employees=mongo.db.employees.find(), welcomes=mongo.db.welcome_password.find())       
 
 
@@ -46,7 +63,7 @@ def new_member():
 
 @app.route('/base')
 def base():
-    
+    print ("working at base")
     return render_template("base.html")
 
 
@@ -57,8 +74,7 @@ def base():
 @app.route('/new_member_info', methods=['POST', 'GET'])   
 def new_member_info():
     if request.method == 'POST':
-        print ("POST working in wrong spot")
-    print ("TESTING ABC")
+        print ("working at new_member_info")
     return render_template("new_member_info.html")    
 
 if __name__ == '__main__':
