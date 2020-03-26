4dab2714e5b2b116a22a8446c4d8cec8be0e6e76
brendanoshea84 <b_oshea_84@hotmail.com>
2020-03-26 18:13:50 +0100

    used update_one to include next of keen in new employees form

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from bson.objectid import ObjectId
 from tkinter import messagebox as mb
 
 app = Flask(__name__)
+app.secret_key='1234'
 app.config["MONGO_DBNAME"] = "milestone3"
 app.config["MONGO_URI"] = os.getenv('MONGO_URI')
 
@@ -20,20 +21,18 @@ def intro():
     # Sign in for new members with a pre password
         if 'new_member_btn' in request.form:
             print("testing")
-
             global new_first_name
             new_first_name = request.form.get('new_member_first')
-
-
             try_pass = request.form.get('new_password')
             w_password = mongo.db.welcome_password.find_one()
+
             for key, val in w_password.items():
                 if 'welcome_password' in key:
                     wel_pass = val
             if wel_pass == try_pass:
                 # If sussecful- Page to input new member info
                 print("testing wel_pass == try_pass")
-                return redirect(url_for('new_member_info'))
+                return redirect(url_for('add_personal_info'))
             else:
                 mb.showinfo("Incorrect Password", "Your welcome password is incorrect, please contact your supervisor! Welcome Password for CODE INSTITUTE: "+ wel_pass)
                 print("welcome password ")
@@ -43,6 +42,7 @@ def intro():
             global login_user
             employees = mongo.db.employees
             login_user =  employees.find_one({'employee_username' : request.form.get('login')}) or employees.find_one({'employee_number' : request.form.get('login')})
+            
             login_pass = request.form.get('password_user')
 
             if login_user:
@@ -55,10 +55,84 @@ def intro():
                 else:
                     print("error at login")
                     return redirect(url_for('error_existing'))
-                
-              
-   
     return render_template('intro.html', employees=mongo.db.employees.find(), welcomes=mongo.db.welcome_password.find())       
+
+
+@app.route('/new_member_info', methods=['POST', 'GET'])   
+def new_member_info():
+        
+    return render_template("/employeeinfo/personal_info.html", employees=mongo.db.employees.find())    
+
+
+@app.route('/add_personal_info', methods=['POST', 'GET'])
+def add_personal_info():
+    # Creating username number and username for new employee
+    # New User employee number
+    new_employee = mongo.db.employees
+    # Find highest employee number
+    employ_number = new_employee.distinct('employee_number')
+    new_number = max(employ_number) 
+    employee_number = int(new_number) +1
+    session['login_user'] = employee_number
+
+    print("testing area 1")
+
+# When form is sent
+    if request.method == 'POST': 
+        if 'personal_info_btn' in request.form:
+        
+            post_data = request.form.to_dict()
+
+        # Create username
+        # Insure input from form is lowercase
+            employee_first_name = request.form['employee_first_name'].lower()
+            employee_last_name = request.form['employee_last_name'].lower()
+            employee_username = (employee_first_name + '.' + employee_last_name)
+        
+            post_data['employee_username'] = employee_username
+            post_data['employee_number'] = employee_number
+            post_data['employee_first_name'] = employee_first_name
+            post_data['employee_last_name'] = employee_last_name
+            print("tesing sending")
+             
+            # Get the id of the new insert to collections 
+            new_id = new_employee.insert_one(post_data)
+            global new_doc_id
+            new_doc_id = new_id.inserted_id
+        return redirect(url_for('emergcy'))
+    return render_template("/employeeinfo/personal_info.html", employees=mongo.db.employees.find(), new_first_name=new_first_name)     
+
+
+@app.route('/qualifications', methods=['POST', 'GET'])
+def qualifications():
+    return render_template("/employeeinfo/emergcy.html")
+
+
+@app.route('/personal_info', methods=['POST', 'GET'])
+def personal_info():
+    return render_template("")
+
+@app.route('/bank_details', methods=['POST', 'GET'])
+def bank_details():
+    return render_template("/employeeinfo/bank_details.html")
+
+
+
+
+@app.route('/emergcy/', methods=['POST', 'GET'])
+def emergcy():
+    if request.method == 'POST':
+        this_user = mongo.db.employees
+        this_user.update_one({'_id': new_doc_id},
+        {'$set': {
+        'employee_next_of_keen': request.form.get('employee_next_of_keen'),
+        'employee_next_of_keen_mob': request.form.get('employee_next_of_keen_mob')
+        }}, upsert= True)
+        return redirect(url_for('qualifications'))
+    return render_template("/employeeinfo/emergcy.html", new_doc_id=new_doc_id)    
+
+
+
 
 
 @app.route('/main', methods=['GET'])
@@ -67,16 +141,10 @@ def main():
     print(login_user)
     return render_template("main.html", testing=login_user)
 
-
-
-
-
 @app.route('/base', methods=['POST', 'GET'])
 def base():
     print ("working at base")
     return render_template("base.html")
-
-
 
 @app.route('/error_existing')
 def error_existing():
@@ -86,38 +154,14 @@ def error_existing():
 
 
 
-@app.route('/new_member_info', methods=['POST', 'GET'])   
-def new_member_info():
-# Creating username number and username for new employee
-    # New User employee number
-    new_employee = mongo.db.employees
-    # Find highest employee number
-    employ_number = new_employee.distinct('employee_number')
-    new_number = max(employ_number) 
-    employee_number = int(new_number) +1
-
-# When form is sent
-    if request.method == 'POST': 
-        post_data = request.form.to_dict()
-        # Create username
-        employee_first_name = request.form['employee_first_name'].lower()
-        employee_last_name = request.form['employee_last_name'].lower()
-        employee_username = (employee_first_name + '.' + employee_last_name)
-        
-        post_data['employee_username'] = employee_username
-        post_data['employee_number'] = employee_number
-        post_data['employee_first_name'] = employee_first_name
-        post_data['employee_last_name'] = employee_last_name
-        new_employee.insert_one(post_data)
-        
-        
-        
-
-    
-    return render_template("new_member_info.html", new_first_name = new_first_name, employees=mongo.db.employees.find())    
+# Log out
+@app.route("/logout")  
+def logout(): 
+    session.pop("user", None) 
+    return redirect(url_for("/intro"))
 
 if __name__ == '__main__':
-    app.secret_key='1234'
+    
     app.run(host=os.environ.get('IP'),
             port=os.environ.get('PORT'),
             debug=True)
