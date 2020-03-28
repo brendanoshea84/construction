2f0fa35445538aa88ee355001f07ef3d94209e87
brendanoshea84 <b_oshea_84@hotmail.com>
2020-03-28 19:18:10 +0100

    if username is taken, allowing a secound verison of username. Password protection

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import bcrypt
 from flask import Flask, render_template, redirect, request, url_for, session
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId 
@@ -10,52 +11,40 @@ app.config["MONGO_URI"] = os.getenv('MONGO_URI')
 
 mongo = PyMongo(app)
 
-
 # Welcome Page
 # Sign in for new members with a pre password
 # Login using your own password
 
 @app.route('/intro', methods=['POST', 'GET'])     
-def intro():  
-                  
-    return render_template('intro.html', employees=mongo.db.employees.find(), welcomes=mongo.db.welcome_password.find())       
+def intro():            
+    return render_template('intro.html', employee = mongo.db.employees)      
 
 @app.route('/', methods=['POST', 'GET'])
 @app.route('/sign_in', methods=['POST', 'GET'])
 def sign_in():
-    print("before post")
     if request.method == 'POST':
     # Login using your own password
-        # User can use they name or employee number
         print("after post")
-        global login_user
         employees = mongo.db.employees
-        login_user =  employees.find_one({'employee_username' : request.form.get('login')})
-        login_pass = request.form.get('password_user')
-
+        login_user =  employees.find_one({'username' : request.form.get('login')})
+        
+        print("before test")
         if login_user:
-            if login_pass == login_user['employ_password']:
+            if bcrypt.hashpw(request.form.get('password_user').encode('utf-8'), login_user['password']) ==  login_user['password']:
                 print("testing login area")
-
-                # Fix here
-                # session['login_user'] = login_user[''_id]
-                    
-                print(session)
-                print('session')
+                session['username'] = request.form['username']
+                
                 return redirect(url_for('main'))
             else:
-                    # Fix here
-                    # Add error msg
+                # Fix here
+                # Add error msg
                 print("error at login")
-    return render_template('intro.html', employees=mongo.db.employees.find(), welcomes=mongo.db.welcome_password.find())       
+    return render_template('intro.html', employee = mongo.db.employees)       
             
-
 @app.route('/sign_up', methods=['POST', 'GET'])
 def sign_up():
     if request.method == 'POST': 
     # Sign in for new members with a pre password
-        
-        print("testing")
         global new_first_name
         new_first_name = request.form.get('new_member_first')
         try_pass = request.form.get('new_password')
@@ -72,117 +61,101 @@ def sign_up():
             # Fix here
             # flash("testing")
             print("welcome password wrong")
-        
-    return render_template('intro.html', employees=mongo.db.employees.find(), welcomes=mongo.db.welcome_password.find())       
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    return render_template('intro.html')       
 
 
 @app.route('/new_member_info', methods=['POST', 'GET'])   
 def new_member_info():
-    print('SOMEW')    
-    return render_template("/employeeinfo/personal_info.html", employees=mongo.db.employees.find())    
-
+       
+    return render_template("/employeeinfo/personal_info.html")    
 
 @app.route('/add_personal_info', methods=['POST', 'GET'])
 def add_personal_info():
     # Creating username number and username for new employee
-    # New User employee number
     new_employee = mongo.db.employees
-    # Find highest employee number
-    employ_number = new_employee.distinct('employee_number')
-    new_number = max(employ_number) 
-    employee_number = int(new_number) +1
-    session['login_user'] = employee_number
-
-    print("testing area 1")
-
+   
 # When form is sent
     if request.method == 'POST': 
         post_data = request.form.to_dict()
-
         # Create username
-        # Insure input from form is lowercase
-       
-        post_data['employee_first_name'] = request.form['employee_first_name'].lower()
-        post_data['employee_last_name'] = request.form['employee_last_name'].lower()
-        post_data['employee_username'] = post_data['employee_first_name'] + '.' + post_data['employee_last_name']
+        # Insure input from form is lowercase  
+        temp_username = request.form['first_name'].lower() + '.' + request.form['last_name'].lower()
+        
+        # Check to see if username has been used
+        temp_user = new_employee.find_one({'username' : temp_username})
 
-        print("tesing sending")
-             
+        if temp_user:
+            # Create another username if username is been used before using dob
+            print("already in database")
+            tempdate = request.form['dob']
+            testdate = tempdate.split('-')
+            year = testdate[0]
+            double_used_username = request.form['first_name'].lower() + '.' + request.form['last_name'].lower() + year
+            double_test = new_employee.find_one({'username' : double_used_username})
+
+            if double_test:
+                # If two usernames are the same, go to a page with suggestions
+                return redirect(url_for('username_wrong'))
+                post_data['username'] = request.form['first_name'].lower() + '.' + request.form['last_name'].lower() + year
+        else:
+            post_data['username'] = temp_username
+
+        post_data['first_name'] = request.form['first_name'].lower()
+        post_data['last_name'] = request.form['last_name'].lower() 
+        post_data['password'] = bcrypt.hashpw(request.form['password'].encode('utf-8'), bcrypt.gensalt())
+        
         # Get the id of the new insert to collections 
         new_id = new_employee.insert_one(post_data)
         global new_doc_id
+
+        # insert to collections
         new_doc_id = new_id.inserted_id
         return redirect(url_for('emergcy'))
-
-    print('HERE IS SOMETHING')
-    return render_template("/employeeinfo/personal_info.html", employees=mongo.db.employees.find(), new_first_name=new_first_name)     
+    return render_template("/employeeinfo/personal_info.html", new_first_name=new_first_name)     
 
 
-@app.route('/qualifications', methods=['POST', 'GET'])
-def qualifications():
-    return render_template("/employeeinfo/emergcy.html")
+@app.route('/bank_details', methods=['POST', 'GET'])
+def bank_details():
+    if request.method == 'POST':
+        # Update to made _id 
+        this_user.update_one({'_id': new_doc_id},
+        {'$set': {
+        'bank_name': request.form.get('bank_name'),
+        'bank_number': request.form.get('bank_number')
+        }}, upsert= True)
+        return redirect(url_for('main'))
+    return render_template("/employeeinfo/bank_details.html", this_user=this_user, new_doc_id=new_doc_id, username =username, new_first_name=new_first_name)
+
+@app.route('/emergcy/', methods=['POST', 'GET'])
+def emergcy():
+    global this_user
+    global username
+    this_user = mongo.db.employees
+    username = this_user.find_one({'_id': new_doc_id})
+    for key, val in username.items():
+        if 'username' in key:
+            username = val
+    print(username)
+    if request.method == 'POST':
+        this_user.update_one({'_id': new_doc_id},
+        {'$set': {
+        'next_of_kin': request.form.get('next_of_kin'),
+        'next_of_kin_mob': request.form.get('next_of_kin_mob')
+        }}, upsert= True)
+        return redirect(url_for('bank_details'))
+    return render_template("/employeeinfo/emergcy.html", new_doc_id=new_doc_id, username =username, new_first_name=new_first_name)    
+
 
 
 @app.route('/personal_info', methods=['POST', 'GET'])
 def personal_info():
     return render_template("")
 
-@app.route('/bank_details', methods=['POST', 'GET'])
-def bank_details():
-    return render_template("/employeeinfo/bank_details.html")
-
-
-
-
-@app.route('/emergcy/', methods=['POST', 'GET'])
-def emergcy():
-    if request.method == 'POST':
-        this_user = mongo.db.employees
-        this_user.update_one({'_id': new_doc_id},
-        {'$set': {
-        'employee_next_of_keen': request.form.get('employee_next_of_keen'),
-        'employee_next_of_keen_mob': request.form.get('employee_next_of_keen_mob')
-        }}, upsert= True)
-        return redirect(url_for('qualifications'))
-    return render_template("/employeeinfo/emergcy.html", new_doc_id=new_doc_id)    
-
-
-
-
-
 @app.route('/main', methods=['GET'])
 def main():
     print ("going to main")
-    print(login_user)
-    return render_template("main.html", testing=login_user)
+    
+    return render_template("main.html")
 
 @app.route('/base', methods=['POST', 'GET'])
 def base():
@@ -191,17 +164,19 @@ def base():
 
 @app.route('/error_existing')
 def error_existing():
-    
     return render_template("error_existing.html")
 
 
 
 
-# Log out
- 
+# If username has been used twice before and this is the third time
+@app.route('/username_wrong')
+def username_wrong():
+    print("too many users, not enough names")
+    return render_template("error_existing.html")    
+
 
 if __name__ == '__main__':
-    
     app.run(host=os.environ.get('IP'),
             port=os.environ.get('PORT'),
             debug=True)
---
+++ static/js/employees_data.js
@@ -0,0 +1,24 @@
+// Change tabs in employees data
+
+let personal_tab = document.getElementById("personal_tab");
+let emergcy_tab = document.getElementById("emergcy_tab");
+let bank_tab = document.getElementById("bank_tab");
+
+$(document).ready(function() {
+    if (window.location.href.indexOf("personal_info") > -1) {
+        personal_tab.classList.add('active')
+        emergcy_tab.classList.remove('active')
+        bank_tab.classList.remove('active')
+        console.log("personal_info");
+    } else if (window.location.href.indexOf("emergcy") > -1) {
+        personal_tab.classList.remove('active')
+        emergcy_tab.classList.add('active')
+        bank_tab.classList.remove('active')
+        console.log("emergcy");
+    } else if (window.location.href.indexOf("bank_details") > -1) {
+        personal_tab.classList.remove('active')
+        emergcy_tab.classList.remove('active')
+        bank_tab.classList.add('active')
+        console.log("bank_details");
+    }
+})
