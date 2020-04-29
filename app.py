74b7f9b271d7869b78068455d6fdfd879252991c
brendanoshea84 <b_oshea_84@hotmail.com>
2020-04-29 10:21:32 +0200

    fixed news in home page

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ if os.path.exists("env.py"):
 app = Flask(__name__, template_folder='templates')
 app.config["MONGO_DBNAME"] = "milestone3"
 app.config["MONGO_URI"] = os.getenv('MONGO_URI')
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 mongo = PyMongo(app)
 
 
